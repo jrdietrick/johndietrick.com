b282b4e7868760088771d2b75eac810ae410f29c
John R Dietrick <john@johndietrick.com>
2015-10-01 00:11:11 +0800

    Only worry about the overlay if it's there

--- static/js/base.js
+++ static/js/base.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     var overlay = document.getElementById('splash_overlay');
-    overlay.onmouseover = function (event) {
-        if (typeof(this.filters) == 'undefined') {
-            this.style.opacity = 1.0;
-        } else {
-            this.filters.alpha.opacity = 80;
+    if (overlay !== null) {
+        overlay.onmouseover = function (event) {
+            if (typeof(this.filters) == 'undefined') {
+                this.style.opacity = 1.0;
+            } else {
+                this.filters.alpha.opacity = 80;
+            }
         }
-    }
-    overlay.onmouseout = function(event) {
-        if (typeof(this.filters) == 'undefined') {
-            this.style.opacity = 0.0;
-        } else {
-            this.filters.alpha.opacity = 0;
+        overlay.onmouseout = function(event) {
+            if (typeof(this.filters) == 'undefined') {
+                this.style.opacity = 0.0;
+            } else {
+                this.filters.alpha.opacity = 0;
+            }
         }
     }
 });
